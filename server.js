4c9db363b724d10df0637857c0468d6162974a00
evansashana <evans.ashana.3b@gmail.com>
2018-10-13 20:21:55 -0400

    server.js is complete and pages should appear

--- server.js
+++ server.js
@@ -1,97 +1,118 @@
+
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var cors = require('cors')
+var cors = require('cors');
 var index = require('./routes/index');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+// require('./models/Users');
+// require('./models/Evt_Admins');
+// require('./models/Events');
+// require('./models/Judges');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var events = require('./routes/events');
+
 // app
 var app = express();
 // cors
 app.use(cors());
 // views. -- VERY IMPORTANT
-app.set('views', path.join(__dirname, 'src'));
+app.set('views', path.join(__dirname, 'views'));
 // engine
-app.set('view enginer', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+//app.engine('view engine', require('ejs').renderFile);
 // angular  dist -- VERY IMPORTANT
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'public')));
 // body bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(logger('dev'));
+
+
+
 // routes
-app.use('/', index);
+//app.use('/', index);
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/events', events);
 // port.
 var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
 
-// var express = require('express');
-// var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
-//
+//var express = require('express');
+//var path = require('path');
+//var cookieParser = require('cookie-parser');
+//var bodyParser = require('body-parser');
+//var mongoose = require('mongoose');
+
 // require('./models/Users');
 // require('./models/Evt_Admins');
 // require('./models/Events');
 // require('./models/Judges');
 //
+//
 // var routes = require('./routes/index');
 // var users = require('./routes/users');
 // var events = require('./routes/events');
-//
-// mongoose.connect("mongodb://localhost:27017/Scored!");
-//
-// var app = express();
-//
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-//
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+
+//mongoose.connect("mongodb://localhost:27017/Scored!");
+
+//var app = express();
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.static(path.join(__dirname, 'public')));
 //
 // app.use('/', routes);
 // app.use('/users', users);
 // app.use('/events', events);
-//
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// // error handlers
-//
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-//
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-//
-//
-// module.exports = app;
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
