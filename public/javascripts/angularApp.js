883b36d1b08f332b63aeaa7ad517df0f13672f18
evansashana <evans.ashana.3b@gmail.com>
2018-11-24 14:38:33 -0500

    added a confirm dialog to check if they want to submit scores

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -386,22 +386,6 @@ app.controller('UserCtrl', ['$scope', '$rootScope', '$location', 'USER_ROLES', '
           alert("You can now judge this event: " + $scope.event.name);
           $location.path('/judge/event_form');
 
-          $scope.sendEmail = function () {
-            var message = "Hello,\n\nHere are the results of " + event.name +" held at " + event.location + " on " +
-              "November" + " " + "26" + ", " +
-              "2018" + " starting at " + "4:30 PM" + "\n\nTeam Scores: \n\n Fun-Recyclist: " +
-              "Difficulty: 7 Presentation: 8\n\n" +
-              "Fairy-Whales: Difficulty: 4 Presentation: 6" +
-              "\n\n Watchable: Difficulty: 8 Presentation: 10" +
-              "\n\n NaN: Difficulty: 8 Presentation: 6" +
-              "\n\n 1st Place: Watchable"+ "\n\n 2nd Place: NaN" + "\n\n 3rd Place: Fun-Recyclist"+
-              "\n\n Thanks for using our App JAHA! Administration";
-
-            $scope.sendEmail("contactus.scored@gmail.com", $rootScope.currentUserData.email, "Results!", message);
-            $scope.sendEmail("contactus.scored@gmail.com", 'evans.ashana.3b@gmail.com', "Results!", message);
-
-            $rootScope.sendEmail("contactus.scored@gmail.com", 'evans.ashana.3b@gmail.com', "Results!", message);
-          }
 
 
         }, function(res) {
@@ -414,6 +398,37 @@ app.controller('UserCtrl', ['$scope', '$rootScope', '$location', 'USER_ROLES', '
       }
 
 
+      $scope.directive('ngConfirmClick', [
+        function(){
+          return {
+            link: function (scope, element, attr) {
+              var msg = attr.ngConfirmClick || "Are you sure?";
+              var clickAction = attr.confirmedClick;
+              element.bind('click',function (event) {
+                if ( window.confirm(msg) ) {
+                  scope.$eval(clickAction)
+                }
+              });
+            }
+          };
+        }]);
+
+        $scope.sendEmail = function () {
+          var message = "Hello,\n\nHere are the results of " + event.name +" held at " + event.location + " on " +
+            "November" + " " + "26" + ", " +
+            "2018" + " starting at " + "4:30 PM" + "\n\nTeam Scores: \n\n Fun-Recyclist: " +
+            "Difficulty: 7 Presentation: 8\n\n" +
+            "Fairy-Whales: Difficulty: 4 Presentation: 6" +
+            "\n\n Watchable: Difficulty: 8 Presentation: 10" +
+            "\n\n NaN: Difficulty: 8 Presentation: 6" +
+            "\n\n 1st Place: Watchable"+ "\n\n 2nd Place: NaN" + "\n\n 3rd Place: Fun-Recyclist"+
+            "\n\n Thanks for using our App JAHA! Administration";
+
+          $scope.sendEmail("contactus.scored@gmail.com", $rootScope.currentUserData.email, "Results!", message);
+          $scope.sendEmail("contactus.scored@gmail.com", 'evans.ashana.3b@gmail.com', "Results!", message);
+
+          $rootScope.sendEmail("contactus.scored@gmail.com", 'evans.ashana.3b@gmail.com', "Results!", message);
+        }
 
         /*$scope.populateForm = function() {
             /*var el = document.createElement('div');
