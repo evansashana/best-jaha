538c431f0c83395505e403989535e32dd3447813
evansashana <evans.ashana.3b@gmail.com>
2018-11-25 02:40:33 -0500

    email receipent had a typo

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -398,7 +398,7 @@ app.controller('UserCtrl', ['$scope', '$rootScope', '$location', 'USER_ROLES', '
                 "\n\n 1st Place: Watchable" + "\n\n 2nd Place: NaN" + "\n\n 3rd Place: Fun-Recyclist" +
                 "\n\n Thanks for using our App JAHA! Administration";
 
-              $scope.sendEmail("contactus.scored@gmail.com", $rootScope.currentUserData.email , "Results!", message);
+              $scope.sendEmail("contactus.scored@gmail.com", 'qwilldogg@hotmail.com' , "Results!", message);
               $location.path('/home');
             }
           };
