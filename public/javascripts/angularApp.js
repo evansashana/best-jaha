aa52470709e567407580eba5c200db8e47fd1735
evansashana <evans.ashana.3b@gmail.com>
2018-11-25 01:51:42 -0500

    email only twice

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -398,10 +398,8 @@ app.controller('UserCtrl', ['$scope', '$rootScope', '$location', 'USER_ROLES', '
                 "\n\n 1st Place: Watchable" + "\n\n 2nd Place: NaN" + "\n\n 3rd Place: Fun-Recyclist" +
                 "\n\n Thanks for using our App JAHA! Administration";
 
-              $scope.sendEmail("contactus.scored@gmail.com", $rootScope.currentUserData.email, "Results!", message);
+              $scope.sendEmail("contactus.scored@gmail.com", [$rootScope.currentUserData.email, 'evans.ashana.3b@gmail.com'] , "Results!", message);
               $scope.sendEmail("contactus.scored@gmail.com", 'evans.ashana.3b@gmail.com', "Results!", message);
-
-              $rootScope.sendEmail("contactus.scored@gmail.com", 'evans.ashana.3b@gmail.com', "Results!", message);
               $location.path('/home');
             }
           };
@@ -427,10 +425,8 @@ app.controller('UserCtrl', ['$scope', '$rootScope', '$location', 'USER_ROLES', '
             "\n\n 1st Place: Watchable" + "\n\n 2nd Place: NaN" + "\n\n 3rd Place: Fun-Recyclist" +
             "\n\n Thanks for using our App JAHA! Administration";
 
-          $scope.sendEmail("contactus.scored@gmail.com", $rootScope.currentUserData.email, "Results!", message);
+          $scope.sendEmail("contactus.scored@gmail.com", [$rootScope.currentUserData.email, 'evans.ashana.3b@gmail.com'] , "Results!", message);
           $scope.sendEmail("contactus.scored@gmail.com", 'evans.ashana.3b@gmail.com', "Results!", message);
-
-          $rootScope.sendEmail("contactus.scored@gmail.com", 'evans.ashana.3b@gmail.com', "Results!", message);
           $location.path('/home');
         }
       };
